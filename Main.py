2983ab1e671829bad6a33f4c75bc32819a6298ef
Nicolás <con23197@uvg.edu.gt>
2024-05-08 06:54:04 -0600

    funcion para cargar el archivo txt

--- Main.py
+++ Main.py
@@ -6,5 +6,17 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def cargarRutas(archivo):
+    grafo = nx.Graph()
+    with open(archivo, "r", encoding="utf-8") as f:
+        for linea in f:
+            linea = linea.replace('“', '"').replace('”', '"')
+            datos = linea.strip().split(', ')
+            origen = datos[0].strip('"').strip("'")
+            destino = datos[1].strip('"').strip("'")
+            costo = int(datos[2])
+            grafo.add_edge(origen, destino, weight=costo)
+    return grafo
 
+Grafo = cargarRutas("rutas.txt")
 
